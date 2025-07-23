f1c947a3d6176fba1824ab17e6fc6228d8b915f1
glowsoony <84847356+glowsoony@users.noreply.github.com>
2025-07-23 16:18:32 -0400

    mal

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2131,7 +2131,7 @@ class PlayState extends MusicBeatState
             if (SONG.needsVoices)
             {
                 var playerVocals = Paths.voices(songData.song, (boyfriend.vocalsFile == null || boyfriend.vocalsFile.length < 1) ? 'Player' : boyfriend.vocalsFile, PlayState.SONG.props.vocalPrefix, PlayState.SONG.props.vocalSuffix);
-                vocals.loadEmbedded(playerVocals != null ? playerVocals : Paths.voices(songData.song, PlayState.SONG.props.vocalPrefix, PlayState.SONG.props.vocalSuffix));
+                vocals.loadEmbedded(playerVocals != null ? playerVocals : Paths.voices(songData.song, null, PlayState.SONG.props.vocalPrefix, PlayState.SONG.props.vocalSuffix));
                 
                 var oppVocals:openfl.media.Sound = cast Paths.voices(songData.song, (dad.vocalsFile == null || dad.vocalsFile.length < 1) ? 'Opponent' : dad.vocalsFile, PlayState.SONG.props.vocalPrefix, PlayState.SONG.props.vocalSuffix);
                 if(oppVocals != null) opponentVocals.loadEmbedded(oppVocals);
